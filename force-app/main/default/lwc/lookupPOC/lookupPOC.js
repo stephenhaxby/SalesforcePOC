9f0bcc927e8753c232ccbc20db0a6421ae423ab0
stephenhaxby <stephen.haxby@icloud.com>
2020-04-24 16:02:50 +1000

    Adding more info...

--- force-app/main/default/lwc/lookupPOC/lookupPOC.js
+++ force-app/main/default/lwc/lookupPOC/lookupPOC.js
@@ -5,12 +5,40 @@ export default class LookupPOC extends LightningElement {
     @track selectedRecordId;
     @track validApplication = true;
 
+    @track lotSize;
+    @track lotSizeDisplay = '';
+    @track zoning;
+    @track zoningDisplay = '';
+
+    @track metropolitanPlanningLevyCertificateRequired = false;
+
+    @track numberOfDwellings;
+    @track estimatedCost;
+
     recordSelected(event){
-        console.log(event.detail.canceled);
-        console.log(event.detail.recordId);
-        console.log(event.detail.value);
+        // console.log(event.detail.canceled);
+        // console.log(event.detail.recordId);
+        // console.log(event.detail.value);
 
         this.selectedRecordId = event.detail.recordId;
+
+        //TODO: To come from MuleSoft Integration
+        this.lotSize = 290;
+        this.lotSizeDisplay = this.lotSize + 'm2';
+
+        this.zoning = 'CDZ1'
+        this.zoningDisplay = 'Comprehensive Development Zone (Schedule 1) Epping';
+    }
+
+    get displayPropertyInformation(){
+        return this.selectedRecordId;
+    }
+
+    get displayRequiredToApply(){
+        console.log('displayRequiredToApply');
+        return this.selectedRecordId
+            && this.numberOfDwellings
+            && this.estimatedCost;
     }
 
     handleSaveClick(event){
@@ -24,9 +52,18 @@ export default class LookupPOC extends LightningElement {
         else {
             this.validApplication = true;
         }
+
+        this.numberOfDwellings = event.detail.value;
     }
 
     estimatedCostChange(event){
+        this.estimatedCost = event.detail.value;
 
+        if(event.detail.value){
+            this.metropolitanPlanningLevyCertificateRequired = event.detail.value > 1076000;
+        }
+        else {
+            this.metropolitanPlanningLevyCertificateRequired = false;
+        }
     }
 }
